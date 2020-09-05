4f9cac7f98a87ae47a8bac7bfa25954cd968431a
Dung Nguyen <dungnguyen9599@gmail.com>
2020-09-05 10:41:26 +0700

    sidebar component static version

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import Widgets from '../../components/Widgets';
+import Feed from './Feed';
 import MessageSender from './MessageSender';
 import Post from './Post';
 import ReelStory from './ReelStory';
 
+import './Home.css';
+
 function Home() {
   return (
-    <div>
-      <p>Hello, this is Home</p>
+    <div className="home">
+      <div className="home__left">
+        <Sidebar />
+      </div>
+      <div className="home__right">
+        <Header />      
+        <Feed />
+      </div>
+      
     </div>
   )
 }
