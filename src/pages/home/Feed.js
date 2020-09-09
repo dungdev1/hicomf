36b58e57a21bc477aa76d6bd25ed718a260257f4
Dung Nguyen <dungnguyen9599@gmail.com>
2020-09-09 12:06:18 +0700

    get posts from firebase cloud and render them.

--- src/pages/home/Feed.js
+++ src/pages/home/Feed.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MessageSender from './MessageSender';
 import Post from './Post';
+
+import db from '../../lib/firebase';
 
 import './Feed.css';
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })))
+      );
+  }, []);
+  console.log(posts);
+
   return (
     <div className="feed">
       <MessageSender />
       <div className="posts">
-        <Post />
-        <Post />
-        <Post />
+        {posts && posts.map(post => (
+          <Post
+            key={post.id}
+            profilePic={post.data.profilePic}
+            image={post.data.image}
+            username={post.data.username}
+            timestamp={post.data.timestamp}
+            caption={post.data.caption}
+            numLike={post.data.numLike}
+            numComment={post.data.numComment}
+            numShare={post.data.numShare}
+          />
+        ))};
       </div>
     </div>
   )
 }
 
-export default Feed
+export default Feed;
--- src/pages/home/Post.js
+++ src/pages/home/Post.js
@@ -4,15 +4,27 @@ import Button from '../../components/Button';
 
 import './Post.css';
 
-function Post() {
+const convertNum = (num) => {
+  if (num > 1000000) {
+    return num / 100000 + "M";
+  }
+  else if (num > 1000) {
+    return num / 1000 + "k";
+  }
+  else {
+    return num;
+  }
+};
+
+function Post({ profilePic, image, username, timestamp, caption, numLike, numComment, numShare }) {
   return (
     <div className="post">
       <div className="post__top">
         <div className="post__info">
-          <Avatar src="" className="post__avatar" />
+          <Avatar src={profilePic} className="post__avatar" />
           <div className="post__topInfo">
-            <h3>Dung Nguyen</h3>
-            <p>Timestamp</p>
+            <h3>{username}</h3>
+            <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
           </div>
         </div>
         <div className="post__setting">
@@ -27,11 +39,12 @@ function Post() {
         </div>
       </div>
       <div className="post__caption">
-        <p>This is the caption for post.</p>
+        <p>{caption}</p>
       </div>
       <div className="post__image">
         {/* <img src="https://s.yimg.com/uu/api/res/1.2/DdytqdFTgtQuxVrHLDdmjQ--~B/aD03MTY7dz0xMDgwO3NtPTE7YXBwaWQ9eXRhY2h5b24-/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae" alt=""/> */}
-        <img src="https://vcdn-vnexpress.vnecdn.net/2020/07/12/VNE-SpaceX-5599-1594554384.jpg" alt="post image" />
+        {/* <img src="https://vcdn-vnexpress.vnecdn.net/2020/07/12/VNE-SpaceX-5599-1594554384.jpg" alt="post image" /> */}
+        <img src={image} alt="post image" />
       </div>
       <div className="post__options">
         <div className="post__option">
@@ -44,7 +57,7 @@ function Post() {
             width="33.161"
             height="29.561"
           />
-          <p style={{color: "#5085E8"}}>25k</p>
+          <p style={{color: "#5085E8"}}>{convertNum(numLike)}</p>
         </div>
         <div className="post__option">
           <Button
@@ -54,7 +67,7 @@ function Post() {
             height="29.314"
             strokeWidth="0.5"
           />
-          <p>2k</p>
+          <p>{convertNum(numComment)}</p>
         </div>
         <div className="post__option">
           <Button
@@ -64,7 +77,7 @@ function Post() {
             height="29.314"
             strokeWidth="0.5"
           />
-          <p>542</p>
+          <p>{convertNum(numShare)}</p>
         </div>
         <div className="post__option"></div>
       </div>
