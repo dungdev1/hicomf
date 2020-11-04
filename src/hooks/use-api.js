085ad2b65df210a6d84288509660b6b009205a3b
Dung Nguyen <dungnguyen9599@gmail.com>
2020-11-04 23:21:05 +0700

    cache data when fetching.

--- src/hooks/use-api.js
+++ src/hooks/use-api.js
@@ -1,6 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 
+const cache = {};
+
 export const useApi = (url, options = {}) => {
   const { getAccessTokenSilently } = useAuth0();
   const [state, setState] = useState({
@@ -11,34 +13,45 @@ export const useApi = (url, options = {}) => {
   const [refreshIndex, setRefreshIndex] = useState(0);
 
   useEffect(() => {
-    setState({
-      ...state,
-      loading: true
-    });
+    if (!url) return;
+
     (async () => {
-      try {
-        const { audience, scope, ...fetchOptions } = options;
-        const accessToken = await getAccessTokenSilently({ audience, scope });
-        const res = await fetch(url, {
-          ...fetchOptions,
-          headers: {
-            ...fetchOptions.headers,
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+      if (cache[url]) {
+        const data = cache[url];
         setState({
           ...state,
-          data: await res.json(),
+          data: data,
           error: null,
           loading: false
         })
-      } catch (error) {
-        setState({
-          ...state,
-          error,
-          loading: false
-        });
+      } else {
+        try {
+          const { audience, scope, ...fetchOptions } = options;
+          const accessToken = await getAccessTokenSilently({ audience, scope });
+          const res = await fetch(url, {
+            ...fetchOptions,
+            headers: {
+              ...fetchOptions.headers,
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          const data = await res.json();
+          cache[url] = data;
+          setState({
+            ...state,
+            data: data,
+            error: null,
+            loading: false
+          })
+        } catch (error) {
+          setState({
+            ...state,
+            error,
+            loading: false
+          });
+        }
       }
+
     })();
   }, [refreshIndex])
 
