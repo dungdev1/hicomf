2e3ac634b5e391ee81f677f917922a52f136aaea
Dung Nguyen <dungnguyen9599@gmail.com>
2020-11-16 11:17:18 +0700

    add fade transition for profile route.

--- src/pages/profile/ProfileMain.js
+++ src/pages/profile/ProfileMain.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+
 import ProfileChronicle from './ProfileChronicle';
 import ProfileFriend from './ProfileFriend';
 import ProfileInformation from './ProfileInformation';
-import './ProfileMain.scss';
 import ProfilePhoto from './ProfilePhoto';
 
+import './ProfileMain.scss';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 function ProfileMain({ relativeUrl }) {
+  const location = useLocation();
   return (
     <div className="ProfileMain">
-      <Route exact path={relativeUrl} component={ProfileChronicle} />
-      <Route path={relativeUrl + '/information'} component={ProfileInformation} />
-      <Route path={relativeUrl + '/friends'} component={ProfileFriend} />
-      <Route path={relativeUrl + '/photos'} component={ProfilePhoto} />
+      <TransitionGroup className="transition-group">
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 100 }}
+          classNames={'fade'}
+        >
+          <section className="route-section">
+            <Switch location={location}>
+              <Route exact path={relativeUrl} component={ProfileChronicle} />
+              <Route path={relativeUrl + '/information'} component={ProfileInformation} />
+              <Route path={relativeUrl + '/friends'} component={ProfileFriend} />
+              <Route path={relativeUrl + '/photos'} component={ProfilePhoto} />
+            </Switch>
+          </section>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 }
